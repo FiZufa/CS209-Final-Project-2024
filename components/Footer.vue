17da1b1c0d7c83cfbcd9fa9abb99b32ba3cf4cdf
Fitria Zu <fi.zufa0612@example.com>
2024-12-21 22:07:14 +0800

    Add footer.vue

---
+++ components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer class="footer">
+      <p>Made with ❤️ by Fitria Zusni and Sreyny Tha</p>
+    </footer>
+  </template>
+  
+  <style scoped>
+  *{
+    font-family: sans-serif;
+  }
+  .footer {
+    text-align: center;
+    padding: 30px;
+    background-color: orange;
+    color: #ccc;
+    margin-top: auto;
+  }
+  </style>
+  
+  
